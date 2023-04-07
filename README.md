a7a5d61d9214a091ad99505ce3e7c5054889dd22
BarkerProoks <jonparkerbrooks@gmail.com>
2023-04-07 11:38:58 -0500

    fixed readme typos, added dockerignore, improved build script

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ with Python's _Flask_ framework.
 Currently, due to time constraints, only the _Docker_ version of this 
 project is supported. You can build it yourself using the provided 
 build script, and then run it on http port 80 using
-`docker run --rm -d -v <local dir>:/var/lib/nginx/public -p 80:80 gmfs`.
+`docker run --rm -d -v <local dir>:/var/lib/nginx/html/public -p 80:80 gmfs`.
 
 # Screenshots
 coming soon
--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-echo "cleaning build space before starting..."
-rm -rfv ./api/**/__pycache__ # clean up any residual __pycache__ files
-rm -rfv ./api/venv
+if [ "$1" == "--clean" ] || [ "$1" == "-c" ]; then
+	echo "cleaning reproducible project files..."
+	rm -rf ./api/**/__pycache__
+	rm -rf ./gmfs/node_modules
+	rm -rf ./gmfs/build
+	rm -rf ./api/venv
+	rm -rf ./build
+	echo "done."
+	exit 0
+fi
+
+if ! [ -d ./gmfs/node_modules ]; then
+	echo "downloading packages..."
+	npm install --prefix ./gmfs
+fi
 
 if ! [ -d ./gmfs/build ]; then
 	echo "building frontend components"
@@ -14,3 +26,9 @@ if ! [ -d ./gmfs/build ]; then
 fi
 
 docker build --rm -t gmfs .
+
+if [ "$1" == "--save" ] || [ "$1" == "-s" ]; then
+	echo "saving build to GZip compressed tarball..."
+	mkdir ./build && docker save gmfs:latest | gzip > ./build/gmfs.tar.gz
+	echo "done."
+fi
